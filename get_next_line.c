0dd7a1386ef5c791131a411140aa6f2ed9116f56
il-iharch <harchilhame@gmail.com>
2024-12-25 18:16:33 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: iharch <iharch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 14:04:48 by iharch            #+#    #+#             */
-/*   Updated: 2024/12/24 11:47:34 by iharch           ###   ########.fr       */
+/*   Updated: 2024/12/25 18:15:10 by iharch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,25 @@ char *ft_read(char *rd, int fd)
 	char *buffer;
 	
 	buffer = NULL;
-	buffer = malloc (sizeof(char) * (ssize_t)BUFFER_SIZE + 1);
+	buffer = malloc ((ssize_t)BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
+	buffer[0] = '\0';
 	num_rd = 1;
-	while (num_rd > 0 && buffer)
+	while (num_rd > 0  && !ft_strchr(rd, '\n'))
 	{
 		num_rd = read(fd, buffer, BUFFER_SIZE);
-		if (num_rd < 0)
-			return (free(buffer), buffer = NULL, NULL);
-		buffer[num_rd] = '\0';
-		rd = ft_strjoin(rd, buffer);
-		if (!rd)
-			return (free(buffer), buffer = NULL, free(rd), rd =  NULL);
-		if (ft_strchr(rd, '\n'))
-			break;
+		if (num_rd > 0)
+		{
+			buffer[num_rd] = '\0';
+			rd = ft_strjoin(rd, buffer);
+			if (!rd)
+				return (free(buffer), buffer = NULL, free(rd), rd =  NULL);
+		}
 	}
 	free (buffer);
+	if (num_rd == -1)
+		return(free(rd), NULL);
 	return (rd);
 }
 char *ft_line(char *rd)
@@ -43,13 +45,14 @@ char *ft_line(char *rd)
 	char *line;
 	
 	i = 0;
-	if (!rd || rd[0] == '\0')
-		return (free(rd), rd = NULL, NULL);
+	if (!rd)
+		return (free(rd),rd = NULL, NULL);
 	while (rd[i] && rd[i] != '\n')
 		i++;
-	i++;
+	if (rd[i] == '\n')
+		i++;
 	line = ft_substr(rd, 0 , i);
-	 if (line[0] == '\0' || !line)
+	if (!line || line[0] == '\0')
 		return (free(line), line = NULL, NULL);
 	return (line);
 }
@@ -60,11 +63,15 @@ char *ft_rest(char *rd)
 	int j;
 	
 	i = 0;
+	if (rd[i] == '\0')
+		return (NULL);
 	while (rd[i] && rd[i] != '\n')
 		i++;
 	i++;
 	j = ft_strlen(rd) - i;
 	str = ft_substr(rd, i, j);
+	if (!str)
+		return (free(rd), NULL);
 	free(rd);
 	return (str);
 }
@@ -72,9 +79,9 @@ char *get_next_line(int fd)
 {
 	static char *rd;
 	char        *line;
-	int num_rd;
+
 	
-	if (fd < 0 || BUFFER_SIZE <= 0 )
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > OPEN_MAX)
 		return (NULL);
 	rd = ft_read(rd, fd);
 	if (!rd)
@@ -84,34 +91,4 @@ char *get_next_line(int fd)
 		return (free(line), line = NULL, free(rd), rd = NULL, NULL);
 	rd = ft_rest(rd);
 	return (line);
-}
-void f()
-{
-	system("leaks");
-}
-int main()
-{
-	// atexit(f);
-	int fd1 = open ("fd", O_CREAT | O_RDWR, 0777);
-	write(fd1, "hello\nworld!",12);
-	int fd2 = open ("fd", O_CREAT | O_RDWR, 0777);
-	write(fd2, "hello\nworld!",12);
-	int fd3= open ("fd", O_CREAT | O_RDWR, 0777);
-	write(fd3, "hello\nworld!",12);
-	
-	int fd4 = open ("fd", O_CREAT | O_RDWR, 0777);
-	write(fd4, "hello\nworld!",12);
-	
-	int fd5 = open ("fd", O_CREAT | O_RDWR, 0777);
-	write(fd5, "hello\nworld!",12);
-	
-	printf("%s",get_next_line(fd1));
-	printf("%s",get_next_line(fd2));
-	printf("%s",get_next_line(fd3));
-	printf("%s",get_next_line(fd4));
-	printf("%s",get_next_line(fd5));
-	
-	// printf("%s",s);
-   
-	
 }
