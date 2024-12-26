0dbd43054f440ff0e048518027084b977b59f5db
il-iharch <harchilhame@gmail.com>
2024-12-26 17:12:56 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,28 +6,29 @@
 /*   By: iharch <iharch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 17:45:33 by iharch            #+#    #+#             */
-/*   Updated: 2024/12/21 17:47:59 by iharch           ###   ########.fr       */
+/*   Updated: 2024/12/26 17:08:21 by iharch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GETT_NEXT
-# define GETT_NEXT
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
+#  define BUFFER_SIZE 10
+
 # endif
 
-# include <stdio.h>
-# include <string.h>
+
+
 # include <stdlib.h>
-# include <fcntl.h>
 # include <unistd.h>
+# include <limits.h>
 
 char	*get_next_line(int fd);
-char	*ft_strdup(const char *s1);
+char	*ft_strdup(char *s1);
 size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr(char *s, unsigned int start, size_t len);
 
 #endif
